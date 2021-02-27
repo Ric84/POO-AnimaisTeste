42579727fc93befedc6ec054442649c550f5d40d
Ric84 <ricowsartorii@gmail.com>
2021-02-27 10:01:30 -0300

    rspec minimamente ok

---
+++ spec/main.rb
@@ -0,0 +1,100 @@
+class Gato
+    attr_accessor :habitatCaseiro, :atacaHumanos
+    def initialize(habitatCaseiro, atacaHumanos)
+        @habitatCaseiro = habitatCaseiro
+        @atacaHumanos = atacaHumanos
+    end
+    
+    def darBote()
+        puts "Voce acabou de levar uma mordida!"
+    end
+ 
+end
+
+
+
+
+
+
+
+
+=begin
+Atributos
+Booleano HabitatCaseiro
+Booleano Ataca Humanos
+String Alimentacao
+String Som que faz
+String Cor
+Int Idade 
+
+
+
+
+
+
+class Animal
+    attr_accessor :habitatCaseiro, :atacahumanos
+    def initialize(HabitatCaseiro, AtacaHumanos)
+        @HabitatCaseiro = HabitatCaseiro
+        @AtacaHumanos = Ataca Humanos
+    end
+ 
+    def darBote()
+        puts "Voce acabou de levar uma mordida!"
+    end
+ 
+end
+
+
+
+class Animal caseiro
+    attr_accessor :Cor, :Idade
+    def initialize(Cor, Idade)
+        @cor = Cor
+        @idade = Idade
+    end
+    
+    def darBote()
+        puts "Voce acabou de brincar com ele!"
+    end
+ 
+end
+
+
+
+class Cachorro
+    attr_accessor :Alimentacao, :Som que faz
+    def initialize(Alimentacao, Som que faz)
+        @alimentacao = Alimentacao
+        @somquefaz = Som que faz
+    end
+    
+    def darBote()
+        puts "Voce acabou de escutar o cachorro com fome!"
+    end
+ 
+end
+
+
+
+
+
+
+
+class Leao
+    attr_accessor :Ataca Humanos, :Idade
+    def initialize(Ataca Humanos, Idade)
+        @juba = Ataca Humanos
+        @brabo = Idade
+    end
+    
+    def darBote()
+        puts "Voce acabou de levar uma patada!"
+    end
+ 
+end
+
+=end
+
+
+
